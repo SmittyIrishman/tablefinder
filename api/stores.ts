992904caf30bec2c7619b468c32af3d2b4d0d3c9
SmittyIrishman <troycallowaysmith@gmail.com>
2026-03-05 23:41:27 -0500

    Update stores.ts

--- api/stores.ts
+++ api/stores.ts
@@ -14,11 +14,31 @@ export default async function handler(req: Request) {
   const { lat, lng } = await req.json();
   const API_KEY = process.env.GOOGLE_PLACES_API_KEY;
 
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=80000&keyword=game+store&key=${API_KEY}`;
+  const response = await fetch(
+    `https://places.googleapis.com/v1/places:searchNearby`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY!,
+        "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,places.userRatingCount,places.regularOpeningHours,places.id,places.googleMapsUri",
+      },
+      body: JSON.stringify({
+        includedTypes: ["store"],
+        maxResultCount: 15,
+        locationRestriction: {
+          circle: {
+            center: { latitude: lat, longitude: lng },
+            radius: 80000.0
+          }
+        },
+        rankPreference: "DISTANCE",
+        textQuery: "tabletop game store"
+      })
+    }
+  );
 
-  const response = await fetch(url);
   const data = await response.json();
-
   return new Response(JSON.stringify(data), {
     status: 200,
     headers: { ...corsHeaders, "Content-Type": "application/json" },
